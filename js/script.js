b838807a1dbedbc054d905c7411dbff61a9bb977
이지은 <tori@ryanlab.kr>
2025-05-28 20:30:44 +0900

    concept 영역 scrollTrigger 제작

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function () {
             end: "bottom top",
             scrub: 1,
             pin: true,
-            markers: true,
+            // markers: true,
         },
     })
         .to(".visual h1", { opacity: "1", ease: "none", duration: 10 }, 5)
@@ -18,4 +18,32 @@ $(function () {
             { scale: "0.4", ease: "none", duration: "10", opacity: 0.3 },
             5
         );
+
+    // imgBox
+    gsap.utils.toArray(".imgBox").forEach(function (imgBox) {
+        // toArray => 인스턴스를 배열로 만드는 메소드
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: imgBox,
+                start: "50% 100%",
+                toggleClass: { targets: imgBox, className: "active" },
+                scrub: 1,
+                // markers: true,
+            },
+        });
+    });
+
+    // textBox
+    gsap.utils.toArray(".textBox").forEach(function (textBox) {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: textBox,
+                start: "50% 80%",
+                end: "100% 0%",
+                toggleClass: { targets: textBox, className: "active" },
+                scrub: 1,
+                // markers: true,
+            },
+        });
+    });
 });
